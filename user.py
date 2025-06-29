911fc09cf0f106aad2908ee21754bcee76a8403d
mahad2810 <164727402+mahad2810@users.noreply.github.com>
2025-06-29 11:44:50 +0530

    c2

--- user.py
+++ user.py
@@ -1,13 +1,9 @@
 import requests
-
 url = "https://wss1.mtsp.co.in:15207/user/profile"
-
 headers = {
   "Api-Version": "3",
   "Authorization": "ijTLVhyDdou1iyK5MLnJmyAwT:T>k53|N3WBtb*bT.415%"
 }
-
 response = requests.get(url, headers=headers)
-
 data = response.json()
 print(data)
